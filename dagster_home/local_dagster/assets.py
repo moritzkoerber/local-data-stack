556c00607082938400ede7b48aac48066031a037
Moritz Körber <koerber.moritz@gmail.com>
2025-06-02 21:17:26 +0200

    Working version

--- dagster_home/local_dagster/assets.py
+++ dagster_home/local_dagster/assets.py
@@ -1,22 +1,27 @@
+import duckdb
 from dagster import AssetExecutionContext, asset
 from dagster_dbt import DbtCliResource, dbt_assets
-import duckdb
-import os
 
 from .project import duckdb_project
 
 
 @asset(compute_kind="python")
 def area1(context: AssetExecutionContext) -> None:
-    connection = duckdb.connect(":memory:")
-    connection.execute("create schema if not exists local")
+    connection = duckdb.connect("../db.db")
+    table_name = "area1"
+    schema = "local"
+    connection.execute(f"create schema if not exists {schema}")
 
     # Read from the parquet file and create a table
     parquet_path = "../data/area1_small.parquet"
-    connection.execute(f"create or replace table local.test as select * from read_parquet('{parquet_path}')")
+    connection.execute(
+        f"create or replace table {schema}.{table_name} as select * from read_parquet('{parquet_path}')"
+    )
 
     # Get row count for metadata
-    result = connection.execute("select count(*) from local.test").fetchone()
+    result = connection.execute(
+        f"select count(*) from {schema}.{table_name}"
+    ).fetchone()
     # Log some metadata about the table we just wrote
     context.add_output_metadata({"num_rows": result[0]})
 
--- dagster_home/local_dagster/definitions.py
+++ dagster_home/local_dagster/definitions.py
@@ -1,11 +1,12 @@
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
+
 from .assets import area1, duckdb_dbt_assets
 from .project import duckdb_project
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[duckdb_dbt_assets,area1],
+    assets=[duckdb_dbt_assets, area1],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=duckdb_project),
