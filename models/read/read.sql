85e906637b2670048e4c86730ad6c3b2392a69b3
Moritz Körber <koerber.moritz@gmail.com>
2025-08-31 20:40:30 +0200

    Update models

--- models/read/read.sql
+++ models/read/read.sql
@@ -1,4 +1,30 @@
 SELECT
-    *,
-    current_date() AS run_date
+    cases,
+    deaths,
+    recovered,
+    weekincidence,
+    casesper100k,
+    casesperweek,
+    deathsperweek,
+    "delta.cases" AS delta_cases,
+    "delta.deaths" AS delta_deaths,
+    "delta.recovered" AS delta_recovered,
+    "delta.weekIncidence" AS delta_weekincidence,
+    "r.value" AS r_value,
+    "r.rValue4Days.value" AS r_rvalue4days_value,
+    "r.rValue7Days.value" AS r_rvalue7days_value,
+    "hospitalization.cases7Days" AS hospitalization_cases7days,
+    "hospitalization.incidence7Days" AS hospitalization_incidence7days,
+    "meta.source" AS meta_source,
+    "meta.contact" AS meta_contact,
+    "meta.info" AS meta_info,
+    CAST("r.rValue4Days.date" AS TIMESTAMP) AS r_rvalue4days_date,
+    CAST("r.rValue7Days.date" AS TIMESTAMP) AS r_rvalue7days_date,
+    CAST("r.lastUpdate" AS TIMESTAMP) AS r_lastupdate,
+    CAST("hospitalization.date" AS TIMESTAMP) AS hospitalization_date,
+    CAST("hospitalization.lastUpdate" AS TIMESTAMP)
+        AS hospitalization_lastupdate,
+    CAST("meta.lastUpdate" AS TIMESTAMP) AS meta_lastupdate,
+    CAST("meta.lastCheckedForUpdate" AS TIMESTAMP) AS meta_lastcheckedforupdate,
+    CURRENT_DATE() AS run_date
 FROM {{ source("covid19", "germany") }}
