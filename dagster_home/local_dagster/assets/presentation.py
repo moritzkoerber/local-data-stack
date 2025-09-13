e6e8b0bf2b5acea337c252bc6f1895e531888603
Moritz Körber <koerber.moritz@gmail.com>
2025-09-13 13:45:08 +0200

    Working version

--- dagster_home/local_dagster/assets/presentation.py
+++ dagster_home/local_dagster/assets/presentation.py
@@ -15,15 +15,18 @@ from .dbt import dbt_assets
 
 @asset(
     kinds={"python"},
+    group_name="presentation",
+    key_prefix=["presentation"],
     deps=[get_asset_key_for_model([dbt_assets], "gold_covid")],
 )
-def cases_histogram(context: AssetExecutionContext, duckdb: DuckDBResource) -> None:
+def cases_barchart(context: AssetExecutionContext, duckdb: DuckDBResource) -> None:
     with duckdb.get_connection() as conn:
-        cases = conn.sql("select * from gold_covid").pl()
+        cases = conn.sql("select * from main.gold_covid").pl()
 
-        fig = px.histogram(cases, x="cases")
+        fig = px.bar(
+            cases, x="meta_lastupdate", y="cases", title="COVID Cases Over Time"
+        )
         fig.update_layout(bargap=0.2)
-        fig.update_xaxes(categoryorder="total ascending")
         save_chart_path = Path(duckdb.database).parent.joinpath("cases_chart.html")
         fig.write_html(save_chart_path, auto_open=True)
 
--- dagster_home/local_dagster/definitions.py
+++ dagster_home/local_dagster/definitions.py
@@ -6,12 +6,12 @@ from dagster_duckdb import DuckDBResource
 
 from .assets.bronze import covid19_data_rki
 from .assets.dbt import dbt_assets
-from .assets.output import cases_histogram
+from .assets.presentation import cases_barchart
 from .project import local_dagster
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[dbt_assets, covid19_data_rki, cases_histogram],
+    assets=[dbt_assets, covid19_data_rki, cases_barchart],
     schedules=schedules,
     # In Dagster, Resources are the external services, tools, and storage backends
     # you need to do your job. For the storage backend in this project, we'll use DuckDB
