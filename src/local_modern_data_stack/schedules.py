e6ec2d8c96264b3042b402afc79091e903d57dc0
Moritz Körber <koerber.moritz@gmail.com>
2025-10-20 10:04:15 +0200

    Add schedule

--- src/local_modern_data_stack/schedules.py
+++ src/local_modern_data_stack/schedules.py
@@ -3,12 +3,15 @@ This module defines schedules for automatically running Dagster jobs, such as
 materializing dbt models at specified times.
 """
 
-from dagster import AssetSelection, ScheduleDefinition
+from dagster import DefaultScheduleStatus, ScheduleDefinition
+
+from .defs.jobs import all_assets_job
 
 daily_update_schedule = ScheduleDefinition(
     name="daily_update_covid19_data_rki",
-    target=AssetSelection.assets(["bronze", "covid19_data_rki"]),
+    job=all_assets_job,
     cron_schedule="0 0 * * *",
+    default_status=DefaultScheduleStatus.RUNNING,
 )
 
 schedules = [daily_update_schedule]
